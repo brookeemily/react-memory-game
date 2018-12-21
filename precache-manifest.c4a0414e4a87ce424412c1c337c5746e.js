cd0ddd9521534de069ab736f034b317cabac8a71
brooke <brooke.mitchell@comcast.net>
2018-12-21 02:26:48 -0500

    Updates

---
+++ precache-manifest.c4a0414e4a87ce424412c1c337c5746e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efc8a0bed168c5c6af91",
+    "url": "/react-memory-game/static/js/runtime~main.efc8a0be.js"
+  },
+  {
+    "revision": "1ca002994e2d6edb4119",
+    "url": "/react-memory-game/static/js/main.1ca00299.chunk.js"
+  },
+  {
+    "revision": "61b63230e5b0e76c90bd",
+    "url": "/react-memory-game/static/js/1.61b63230.chunk.js"
+  },
+  {
+    "revision": "1ca002994e2d6edb4119",
+    "url": "/react-memory-game/static/css/main.10877040.chunk.css"
+  },
+  {
+    "revision": "57f37894b593e20f86d262c5f65bffbc",
+    "url": "/react-memory-game/index.html"
+  }
+];
